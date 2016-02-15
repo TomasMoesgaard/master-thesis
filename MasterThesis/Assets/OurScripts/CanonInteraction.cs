384eff0e4a572f1253d124d8a0abe1cf211afade
Mads Hagemann Nielsen <Mads Hagemann Nielsen>
2016-02-15 15:29:45 +0100

    Committing Scene and New Animatons

--- MasterThesis/Assets/OurScripts/CanonInteraction.cs
+++ MasterThesis/Assets/OurScripts/CanonInteraction.cs
@@ -1,42 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CanonInteraction : MonoBehaviour {
+public class CanonInteraction : MonoBehaviour
+{
 
     public Transform CanonLoad;
     public Transform CanonFire;
     public Animator AnimLoad;
     public Animator AnimFire;
 
-   	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         AnimLoad = CanonLoad.GetComponent<Animator>();
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-       
-            if (Input.GetMouseButtonDown(0))
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
             {
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out hit))
-                {
                 if (hit.collider.name == "CanonLoad")
                 {
                     AnimLoad.SetTrigger("Open");
-                }                }
-                if (AnimLoad.GetCurrentAnimatorStateInfo(0).IsName("Open"))
-                {
-                    AnimLoad.SetTrigger("Close");
                 }
-                if (AnimLoad.GetCurrentAnimatorStateInfo(0).IsName("Close"))
-                {
-                    AnimLoad.SetTrigger("ReadyToFire");
-                }        
+            }
+            if (AnimLoad.GetCurrentAnimatorStateInfo(0).IsName("Open"))
+            {
+                AnimLoad.SetTrigger("Close");
+            }
+            if (AnimLoad.GetCurrentAnimatorStateInfo(0).IsName("Close"))
+            {
+                AnimLoad.SetTrigger("ReadyToFire");
+            }
         }
     }
- }
+}
 
